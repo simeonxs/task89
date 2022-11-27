8fb2e3f2bcbba0688db0a531d38ced3603cbd789
simeonxs <119071613+simeonxs@users.noreply.github.com>
2022-11-27 15:54:41 +0200

    Update Application.js

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,5 +1,6 @@
 import EventEmitter from "eventemitter3";
 import image from "../images/planet.svg";
+import 'regenerator-runtime/runtime'
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -10,18 +11,48 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
+    this._loading = document.querySelector("progress")
+    this._startLoading();
+    this.emit(Application.events.READY);
+  }
 
+   async _load(){
+    const API_URL = "https://swapi.boom.dev/api/planets"
+    let planets = [];
+    const response = await fetch(API_URL)
+    let {next, results } = await response.json();
+
+    planets = [...results]
+    
+    while (next !== null) {
+      const response = await fetch(next)
+      const result = await response.json();
+      planets = [...planets, ...result.results]
+      next = result.next;
+    }
+
+    planets.forEach(({name, terrain, population}) => this._create(name,terrain,population) )
+    this._stopLoading();
+  }
+
+  _create(name, terrain, population){
     const box = document.createElement("div");
     box.classList.add("box");
     box.innerHTML = this._render({
-      name: "Placeholder",
-      terrain: "placeholder",
-      population: 0,
+      name: name,
+      terrain: terrain,
+      population: population,
     });
 
     document.body.querySelector(".main").appendChild(box);
+  }
 
-    this.emit(Application.events.READY);
+  async _startLoading(){
+    await this._load();
+  }
+
+  _stopLoading(){
+    this._loading.style.visibility = "hidden"
   }
 
   _render({ name, terrain, population }) {
